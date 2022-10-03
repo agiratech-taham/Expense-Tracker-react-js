8dfef0f7274ee68db313351f8d584886b35140c4
agiratech-taham <taha.murtaza@agiratech.com>
2022-10-03 18:10:43 +0530

    Working with input functionalities using state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,20 +6,19 @@ import Card from '../UI/Card';
 
 function ExpenseItem(props) {
 
-  useState();
+  const [title,setTitle]=useState(props.title);
 
-  let title = props.title;
 
   function clickHandler(){
-    title = 'Updated!';
-    console.log();
+    setTitle("Updated");
+    console.log(title);
   }
     
   return (
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price">${props.amount}</div>
         </div>
         <button onClick={clickHandler}>Change Title</button>
